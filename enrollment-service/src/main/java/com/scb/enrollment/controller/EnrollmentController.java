9a954d80e410eadf41490db5c04693d6fb6deccf
akadas <akashdas191098@gmail.com>
2025-06-18 12:23:08 +0530

    circuit breaker pattern implemented

--- enrollment-service/src/main/java/com/scb/enrollment/controller/EnrollmentController.java
+++ enrollment-service/src/main/java/com/scb/enrollment/controller/EnrollmentController.java
@@ -3,6 +3,7 @@ package com.scb.enrollment.controller;
 import com.scb.enrollment.Entities.Enrollment;
 import com.scb.enrollment.Request.EnrollmentRequest;
 import com.scb.enrollment.service.EnrollmentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +17,18 @@ public class EnrollmentController {
     private EnrollmentService enrollmentService;
 
     @PostMapping("/")
+    @CircuitBreaker(name = "enrollment-circuit-breaker", fallbackMethod = "EnrollmentFallback")
     public ResponseEntity<Enrollment> saveEnrollment(@RequestBody EnrollmentRequest enrollmentRequest){
         Enrollment enrollment=enrollmentService.doEnrollment(enrollmentRequest);
         return  new ResponseEntity<Enrollment>(enrollment, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Enrollment> EnrollmentFallback(EnrollmentRequest enrollmentRequest, Exception exception){
+        Enrollment enrollment = Enrollment.builder()
+                .courses(000)
+                .student(000)
+                .status("This enrollment is created dummy because some service is down").build();
+        return new ResponseEntity<Enrollment>(enrollment,HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
